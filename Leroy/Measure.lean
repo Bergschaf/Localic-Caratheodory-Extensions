bc2bd202c87768e1b932d149f724c2ab1932282b
Bergschaf <christian.k@netcom-mail.de>
2024-11-30 22:29:31 +0100

    blueprint

--- Leroy/Measure.lean
+++ Leroy/Measure.lean
@@ -5,7 +5,10 @@ import Leroy.Open_Subframes
 variable {X Y E: Type*} [h : Order.Frame X] [Order.Frame Y] [Order.Frame E]
 
 
-def increasingly_filtered (s : Set X) : Prop :=
+def increasingly_filtered (s : Set (Nucleus X)) : Prop :=
+  ∀ (u v : s), ∃ (w : s), u ≤ w ∧ v ≤ w
+
+def increasingly_filtered' (s : Set (Opens X)) : Prop :=
   ∀ (u v : s), ∃ (w : s), u ≤ w ∧ v ≤ w
 
 
@@ -14,9 +17,15 @@ structure Measure where
   empty : toFun ⊥ = 0
   mono : ∀ (U V : Opens X), U ≤ V → toFun U ≤ toFun V
   pseudosymm : toFun (U ⊔ V) = toFun U + toFun V - toFun (U ⊓ V)
-  filtered : ∀ (s : Set X), increasingly_filtered s → toFun (sSup s) = iSup (fun (x : s) ↦ toFun x)
+  filtered : ∀ (s : Set (Opens X)), increasingly_filtered' s → toFun (sSup s) = iSup (fun (x : s) ↦ toFun x)
 
 
 -- subframes???
-noncomputable def caratheodory (m : @Measure X h) (a : Subframe X) : NNReal :=
-  sInf (m.toFun '' {u : X | a ≤ u})
+noncomputable def Measure.caratheodory {m : @Measure X h} (a : Nucleus X) : NNReal :=
+  sInf (m.toFun '' {u : (Opens X) | a ≤ u})
+
+lemma preserves_sup (m : Measure) (X_n : Set (Nucleus X)) (h : increasingly_filtered X_n) : m.caratheodory (sSup X_n) = sSup (m.caratheodory '' X_n) := by
+  simp [sSup, e_V_nucleus, Measure.caratheodory]
+  apply le_antisymm
+  sorry
+  sorry
