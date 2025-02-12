fe72fa52b567d094c4e81f12112a8ea4fe280bfb
Bergschaf <christian.k@netcom-mail.de>
2025-02-12 09:15:04 +0100

    open_stuff

--- Leroy/Nucleus.lean
+++ Leroy/Nucleus.lean
@@ -7,103 +7,16 @@ import Mathlib.Order.Nucleus
 
 namespace Nucleus
 
-variable {X : Type*} [Order.Frame X] (n : Nucleus X)
+variable {X : Type*} [Order.Frame X]
+variable (s : Set (Nucleus X)) (x : X)
+variable {ι : Type*} (f : ι → Nucleus X)
+variable (m n : Nucleus X)
 
-lemma monotone : Monotone n := n.toClosureOperator.monotone
+@[simp] theorem sSup_apply : sSup s x = ⨅ j ∈ upperBounds s, j x := rfl
 
-instance : InfSet (Nucleus X) where
-  sInf s :=
-  { toFun x := ⨅ f ∈ s, f x,
-    map_inf' x y := by
-      simp only [InfHomClass.map_inf, le_antisymm_iff, le_inf_iff, le_iInf_iff]
-      refine ⟨⟨?_, ?_⟩, ?_⟩ <;> rintro f hf
-      · exact iInf₂_le_of_le f hf inf_le_left
-      · exact iInf₂_le_of_le f hf inf_le_right
-      · exact ⟨inf_le_of_left_le <| iInf₂_le f hf, inf_le_of_right_le <| iInf₂_le f hf⟩
-    idempotent' x := iInf₂_mono fun f hf ↦ (f.monotone <| iInf₂_le f hf).trans_eq f.idempotent
-    le_apply' x := by simp [le_apply] }
+@[simp] theorem iSup_apply : iSup f x = ⨅ j ∈ upperBounds (Set.range f), j x := rfl
 
-@[simp] theorem sInf_apply (s : Set (Nucleus X)) (x : X) : sInf s x = ⨅ j ∈ s, j x := rfl
+@[simp] theorem sup_apply : (m ⊔ n) x = ⨅ j ∈ upperBounds {m, n}, j x := by
+  rw [← sSup_pair, sSup_apply]
 
-@[simp] theorem iInf_apply {ι : Type*} (f : ι → (Nucleus X)) (x : X) : iInf f x = ⨅ j, f j x := by
-  rw [iInf, sInf_apply, iInf_range]
-
-instance : CompleteSemilatticeInf (Nucleus X) where
-  sInf_le := by simp +contextual [← coe_le_coe, Pi.le_def, iInf_le_iff]
-  le_sInf := by simp +contextual [← coe_le_coe, Pi.le_def]
-
-instance : CompleteLattice (Nucleus X) := completeLatticeOfCompleteSemilatticeInf (Nucleus X)
-
-@[simp] theorem inf_apply (m n : Nucleus X) (x : X) : (m ⊓ n) x = m x ⊓ n x := by
-  rw [← sInf_pair, sInf_apply, iInf_pair]
-
-
-instance : HImp (Nucleus X) where
-  himp m n :=
-  { toFun x := ⨅ y ≥ x, m y ⇨ n y
-    idempotent' i := by
-      apply le_iInf₂
-      intro j hj
-      have h : (m (m j ⇨ n j)) ⇨ (n (m j ⇨ n j)) = m j ⇨ n j := by
-        have h_fix : n (m j ⇨ n j) = m j ⇨ n j := by
-          refine le_antisymm ?_ le_apply
-          rw [le_himp_iff, himp_eq_sSup]
-          have h1 : n (sSup {w | w ⊓ m j ≤ n j}) ⊓ m j ≤
-              n (sSup {w | w ⊓ m j ≤ n j}) ⊓  n (m j) := by
-            simp only [le_inf_iff, inf_le_left, true_and]
-            exact inf_le_of_right_le le_apply
-          apply le_trans h1
-          rw [← map_inf, sSup_inf_eq, ← @idempotent _ _ n j]
-          gcongr
-          apply iSup₂_le
-          simp [idempotent]
-        rw [h_fix, himp_himp, ← map_inf, inf_of_le_right (le_trans n.le_apply le_himp)]
-      rw [← h]
-      refine iInf₂_le (m j ⇨ n j) ?_
-      simp only [ge_iff_le, le_himp_iff, iInf_inf, iInf_le_iff, le_inf_iff, le_iInf_iff]
-      intro b1 h2
-      rcases h2 j with ⟨h3, h4⟩
-      exact le_trans (by simp[h4]) (h3 (by exact hj))
-    map_inf' i j := by
-      apply le_antisymm
-      · simp only [ge_iff_le, iInf_inf, le_iInf_iff, le_inf_iff, le_himp_iff, iInf_le_iff]
-        refine fun k ↦ ⟨fun h1 f h2 ↦ ?_, fun k h1 f h2 ↦ ?_⟩
-        all_goals rcases h2 k with ⟨h2_1, h2_2⟩;
-        all_goals refine le_trans (inf_of_le_left h2_2).ge (h2_1 ?_)
-        · exact inf_le_of_left_le h1
-        · exact inf_le_of_right_le h1
-      · simp only [ge_iff_le, iInf_inf, le_iInf_iff, le_himp_iff, iInf_le_iff, le_inf_iff]
-        intro k h2 l h3
-        have h8 : k = (i ⊔ k) ⊓ (j ⊔ k) := by simp [inf_sup_left, inf_sup_right, h2]
-        rw [h8, map_inf, le_inf_iff]
-        rcases h3 (i ⊔ k) with ⟨⟨h4, h5⟩, h7⟩
-        apply And.intro
-        · apply le_trans' (h4 le_sup_left)
-          simp only [le_inf_iff, le_refl, true_and]
-          exact Preorder.le_trans _ _ _ h7 (by gcongr; simp)
-        · apply le_trans' (h5 (j ⊔ k) le_sup_left)
-          simp only [le_inf_iff, le_refl, true_and]
-          exact Preorder.le_trans _ _ _ h7 (by gcongr; simp)
-    le_apply' := by
-      simp only [ge_iff_le, le_iInf_iff, le_himp_iff]
-      refine fun _ _ h ↦ inf_le_of_left_le (le_trans h n.le_apply)}
-
-@[simp] theorem himp_apply (m n : Nucleus X) (x : X) : (m ⇨ n) x = ⨅ y ≥ x, m y ⇨ n y := rfl
-
-instance : HeytingAlgebra (Nucleus X) where
-  le_himp_iff _ n _ := by
-    simp [← coe_le_coe, Pi.le_def]
-    exact ⟨fun h i ↦ h i i le_rfl,
-      fun h1 i j _ ↦ le_trans (inf_le_inf_right (n j) (by gcongr)) (h1 j)⟩
-  compl m := m ⇨ ⊥
-  himp_bot m := rfl
-
-instance : Order.Frame (Nucleus X) where
-  __ := instHeytingAlgebra_leroy
-  __ := instCompleteLattice_leroy
----
-
-theorem sSup_apply (s : Set (Nucleus X)) (x : X) : sSup s x = ⨅ j ∈ {j | ∀ a ∈ s, a ≤ j}, j x := by
-  simp [← sInf_upperBounds_eq_csSup, upperBounds]
-
--- TODO iSup_apply
+end Nucleus
--- Leroy/Sublocale.lean
+++ Leroy/Sublocale.lean
@@ -1,6 +1,5 @@
 import Leroy.Nucleus
-import Mathlib.Order.Synonym
-import Mathlib.Tactic.ApplyFun
+--import Mathlib.Tactic.ApplyFun
 variable {E : Type*} [Order.Frame E]
 
 
@@ -12,23 +11,32 @@ instance : FunLike (Sublocale E) E E where
   coe x := x.toFun
   coe_injective' f g h := (by cases f;cases g; simp_all)
 
+lemma Sublocale.le_iff (u v : Sublocale E): u ≤ v ↔ ∀ i, v i ≤ u i := by exact Eq.to_iff rfl
+
 @[ext]
 lemma ext (a b : Sublocale E) (h : ∀ x, a x = b x) : a = b := by
   exact Nucleus.ext h
 
-lemma Sublocale.sup_apply (u v : Sublocale E) (x : E) : (u ⊔ v) x = u x ⊓ v x := by
+@[simp] lemma Sublocale.sSup_apply (s : Set (Sublocale E)) (x : E) : sSup s x = ⨅ j ∈ s, j x := by
+  simp [OrderDual.supSet]
+  exact rfl
+
+@[simp] lemma Sublocale.iSup_apply {ι : Type*} (f : ι → Sublocale E) (x : E) : iSup f x = ⨅ j, f j x := by
+  simp only [iSup, sSup_apply, Set.mem_range, iInf_exists]
+  refine le_antisymm ?_ (by simp; exact fun a => iInf_le (fun j => (f j) x) a)
+  simp only [le_iInf_iff, iInf_le_iff, forall_apply_eq_imp_iff]
+  exact fun i b a => a i
+
+@[simp] lemma Sublocale.sup_apply (u v : Sublocale E) (x : E) : (u ⊔ v) x = u x ⊓ v x := by
   simp [OrderDual.instSup]
   rw [Nucleus.inf_apply]
 
-lemma Nucleus.sup_apply (n m : Nucleus E) (x : E) : (n ⊔ m) x = ⨅ j, ⨅ (_ : n ≤ j ∧ m ≤ j), j x := by
-  simp_rw [← sSup_pair, sSup_apply]
-  simp
-
-lemma Sublocale.min_apply (u v : Sublocale E) (x : E) : (u ⊓ v) x = ⨅ j, ⨅ (_ : j ≤ u ∧ j ≤ v), j x := by
-  simp [OrderDual.instInf]
+lemma Sublocale.min_apply (u v : Sublocale E) (x : E) : (u ⊓ v) x = ⨅ j ∈ lowerBounds {u, v}, j x := by
+  simp only [OrderDual.instInf, Set.Ici_inter_Ici,Set.mem_Ici, sup_le_iff]
   rw [Nucleus.sup_apply]
+  simp only [upperBounds_insert, upperBounds_singleton, Set.Ici_inter_Ici, Set.mem_Ici, sup_le_iff,
+    lowerBounds_insert, lowerBounds_singleton, Set.Iic_inter_Iic, Set.mem_Iic, le_inf_iff]
   exact rfl
-
 
 
 @[ext]
@@ -49,22 +57,49 @@ instance : Coe (Open E) E where
 instance : Coe (Open E) (Sublocale E) where
   coe x := x.toSublocale
 
+instance : LE (Open E) where
+  le x y := x.element ≤ y.element
+
+lemma le_def (x y : Open E): x ≤ y ↔ x.element ≤ y.element := by rfl
+
+instance : PartialOrder (Open E) where
+  le_refl a := by rfl
+  le_trans a b c h1 h2 := by exact Preorder.le_trans a.element b.element c.element h1 h2
+  le_antisymm a b h1 h2 := by ext; exact le_antisymm h1 h2
+
+instance : CompleteSemilatticeSup (Open E) where
+  sSup s := ⟨sSup (Open.element '' s)⟩
+  le_sSup s x h := by
+    simp [LE.le]
+    apply le_sSup (Set.mem_image_of_mem element h)
+  sSup_le s x h := by simpa [LE.le] using h
+
+lemma sSup_def (s : Set (Open E)) : sSup s = ⟨sSup (Open.element '' s)⟩ := rfl
+
+instance : SemilatticeInf (Open E) where
+  inf x y := ⟨x ⊓ y⟩
+  inf_le_left x y := by simp [le_def]
+  inf_le_right x y := by simp [le_def]
+  le_inf x y z h1 h2 := by simp [le_def]; exact ⟨h1, h2⟩
+
+
 @[simp] lemma toSublocale_apply (U : Open E) (x : E) : U.toSublocale x = U.element ⇨ x := rfl
 
 @[simp] lemma apply_self (U : Open E) : U.toSublocale U.element = ⊤ := by simp
 
-lemma le_iff (U V : Open E) : U.toSublocale ≤ V.toSublocale ↔ U.element ≤ V.element := by
+lemma le_iff (U V : Open E) : U ≤ V ↔ U.toSublocale ≤ V := by
   apply Iff.intro
-  . intro h
-    simp [LE.le, Open.toSublocale] at h
-    let h1 := h V.element
-    simp at h1
-    exact h1
-  . intro h
-    simp [LE.le]
-    intro i
-    repeat rw [toSublocale_apply] -- why does simp not work
+  . simp only [Sublocale.le_iff, toSublocale_apply]
+    intro h i
     exact himp_le_himp_right h
+  . simp only [Sublocale.le_iff, toSublocale_apply]
+    intro h
+    let h1 := h V
+    simp only [himp_self, le_himp_iff, le_top, inf_of_le_right] at h1
+    exact h1
 
-lemma preserves_inf (U V : Open E) : U.toSublocale ⊓ V.toSublocale = (⟨U.element ⊓ V.element⟩ : Open E) := by
+lemma preserves_inf (U V : Open E) : (U ⊓ V).toSublocale = U.toSublocale ⊓ V.toSublocale := by
+  sorry
+
+lemma preserves_sSup (s : Set (Open E)) : (sSup s).toSublocale = sSup (Open.toSublocale '' s) := by
   sorry
